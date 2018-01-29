1766d953fba6b9550c3126579346087914aa0c47
Addis <addwon@gmail.com>
2018-01-29 10:15:25 -0500

    Initial commit

---
+++ src/main/java/assignments/fibonnacisequence/FibonnaciSequenceApplication.java
@@ -0,0 +1,12 @@
+package assignments.fibonnacisequence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibonnaciSequenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibonnaciSequenceApplication.class, args);
+	}
+}
---
+++ src/main/java/assignments/fibonnacisequence/MainController.java
@@ -0,0 +1,35 @@
+package assignments.fibonnacisequence;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/getnumber")
+
+    public String fiboSequence(@RequestParam("number") int num) {
+
+        String sequence="";
+        String sumSequence="";
+        int sum=0;
+        int[] feb = new int[num];
+        feb[0] = 0;
+        feb[1] = 1;
+
+        for(int i=2; i < num; i++){
+            feb[i] = feb[i-1] + feb[i-2];
+            sequence+=feb[i]+"  ";
+            if(i%4==0) {
+                for (int j = i; j >=0; --j) {
+                    sum+= feb[j];
+                }
+                sumSequence += sum+" ";
+            }
+        }
+        sequence+="<br/>"+sumSequence;
+        return sequence;
+
+    }
+}
